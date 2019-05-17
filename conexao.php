d3e5eb965319df7a1a3b9985a3773b6be2190f4d
Ricardo Andrade <email@doricardo.com>
2019-05-17 12:55:32 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -1,6 +1,6 @@
 <?php
     try {
-        $conn = new PDO("mysql:host=mydbinstance.custksmrwqt0.us-east-2.rds.amazonaws.com;port=3306;dbname=webdb", "sa", "password");
+        $conn = new PDO("mysql:host=mydbinstance.cp8qvf9logoe.us-east-1.rds.amazonaws.com;port=3306;dbname=webdb", "sa", "password");
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }
     catch(PDOException $e) {
